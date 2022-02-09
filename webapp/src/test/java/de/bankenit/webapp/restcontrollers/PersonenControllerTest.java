e7d04342121c6d6f5a85862471ddb2dcc7057a6b
Jo Wagner <jowagner@limago.de>
2022-02-09 10:52:21 +0100

    FindAllTest

--- webapp/src/test/java/de/bankenit/webapp/restcontrollers/PersonenControllerTest.java
+++ webapp/src/test/java/de/bankenit/webapp/restcontrollers/PersonenControllerTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -63,10 +67,29 @@ public class PersonenControllerTest {
 		
 		ResponseEntity<PersonDTO> entity = restTemplate.getForEntity("/v1/personen/b2e24e74-8686-43ea-baff-d9396b4202e0", PersonDTO.class);
 		
+		
+	
+		
 		PersonDTO personDTO = entity.getBody();
 		
 		assertEquals("John",personDTO.getVorname());
 		assertEquals(HttpStatus.OK,entity.getStatusCode());
+	}
+
+	@Test
+	void test4() throws Exception {
+		
+	    Iterable<Person> personen = List.of(Person.builder().id("1").vorname("John").nachname("Doe").build(),Person.builder().id("2").vorname("John").nachname("Rambo").build());
+	    
+		
+			
+		when(serviceMock.findeAlle()).thenReturn(personen);
+		
+		ResponseEntity<Iterable<PersonDTO>> entity = restTemplate.exchange("/v1/personen",HttpMethod.GET,null, new ParameterizedTypeReference<Iterable<PersonDTO>>() { });
+		
+		
+		assertEquals(HttpStatus.OK,entity.getStatusCode());
+		assertEquals(2,StreamSupport.stream(entity.getBody().spliterator(), false).count());
 	}
 
 
